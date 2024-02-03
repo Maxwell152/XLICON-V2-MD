76fbad467267fc96f1a54d1d0f3df6aa9e32cf4c
Maxwell152 <157252212+Maxwell152@users.noreply.github.com>
2024-02-03 09:56:23 +0100

    Update config.js

--- config.js
+++ config.js
@@ -55,18 +55,18 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'XLICON-V2'
+global.botname = 'MAXWELL-MD'
 global.premium = 'true'
-global.packname = 'XLICON TEAM' 
-global.author = '@XLICON-V2' 
-global.menuvid = 'https://telegra.ph/file/325630f66abc968eda8e2.mp4'
-global.igfg = '▢✓ Follow My channel\nhttps://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e\n' 
+global.packname = 'MAXWELL TEAM' 
+global.author = '@MAXWELL-MD' 
+global.menuvid = 'https://telegra.ph/file/3afb32a4943d15bd99a13.mp4'
+global.igfg = '▢✓ Follow My On Tiktok https://www.tiktok.com/@maxwell_gaming999?_t=8jZPez38wQ2&_r=1'
 global.dygp = 'https://chat.whatsapp.com/BfH0KLkICn2BjmGFMRcGMW'
-global.fgsc = 'https://github.com/salmanytofficial/XLICON-V2-MD' 
-global.fgyt = 'https://youtube.com/@s4salmanyt'
-global.fgpyp = 'https://youtube.com/@s4salmanyt'
-global.fglog = 'XLICON.jpg' 
-global.thumb = fs.readFileSync('./XLICON.jpg')
+global.fgsc = 'https://www.tiktok.com/@maxwell_gaming999?_t=8jZPez38wQ2&_r=1' 
+global.fgyt = 'https://www.tiktok.com/@maxwell_gaming999?_t=8jZPez38wQ2&_r=1'
+global.fgpyp = 'https://www.tiktok.com/@maxwell_gaming999?_t=8jZPez38wQ2&_r=1'
+global.fglog = 'MAXWELL.jpg' 
+global.thumb = fs.readFileSync('./MAXWELL.jpg')
 
 
 global.wait = '*🕣 _XLICON IS LODIND..._*\n*▰▰▰▱▱▱▱▱⭐*'
